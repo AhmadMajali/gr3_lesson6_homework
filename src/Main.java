0d0266e7166b92dc8eb2d236bdef843b183dda7a
AhmadMajali <amajali392@gmail.com>
2022-09-18 18:43:47 +0600

    new commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         Boss boss = new Boss(300,200);
-        boss.weaponType.setWeaponType("Machine");
-        boss.weaponType.setWeaponName("Ak16");
+        boss.weaponType.setWeaponType("Paintball gun");
+        boss.weaponType.setWeaponName("AK-47");
         System.out.println(boss.info());
         Skeleton skeleton = new Skeleton(300,200,33);
-        skeleton.weaponType.setWeaponType("Machine");
-        skeleton.weaponType.setWeaponName("MK416");
+        skeleton.weaponType.setWeaponType("Paintball gun");
+        skeleton.weaponType.setWeaponName("AWM");
         System.out.println(skeleton.info());
         Skeleton skeleton2 = new Skeleton(400, 300,44);
-        skeleton2.weaponType.setWeaponType("Machine");
-        skeleton2.weaponType.setWeaponName("AKM");
+        skeleton2.weaponType.setWeaponType("Paintball gun");
+        skeleton2.weaponType.setWeaponName("AK");
         System.out.println(skeleton2.info());
 }
 }
