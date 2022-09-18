21206695c51da6e27f0c470f81d01b2be31f5f45
AhmadMajali <amajali392@gmail.com>
2022-09-18 18:38:12 +0600

    new commit

--- src/GameEntity.java
+++ src/GameEntity.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class GameEntity {
+public class GameEntity {
+
+    private int Health;
+    private int damage;
+    public int getType() {
+        return Health;
+    }
+
+    public void setType(int type) {
+        this.Health = type;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public GameEntity(int type, int damage) {
+        this.Health = type;
+        this.damage = damage;
+    }
+
 }
